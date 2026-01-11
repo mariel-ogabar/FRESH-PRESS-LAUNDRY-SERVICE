43018176759ba8d90580d38283507fc293bf52d8
Mariel Ogabar <ogabarmariel35@gmail.com>
2026-01-12 04:54:27 +0800

    Updated Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
+use App\Policies\OrderPolicy;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        Gate::policy(Order::class, OrderPolicy::class);
+
+        // This "Super Admin" check ensures Admins bypass all checks
+        Gate::before(function ($user, $ability) {
+                return $user->hasRole('ADMIN') ? true : null;
+            });
+        }
 }
